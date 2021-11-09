300bc5a6443f4e8774836a70aefcf6d7442062b0
korshunov80@mail.ru <korshunov80@mail.ru>
2021-11-09 10:54:48 +0300

    Update module_24_1.cpp
    task #1 is ready

--- module_24_1.cpp
+++ module_24_1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <iomanip>
 
 void newTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bool &activeTask)
 {
@@ -15,9 +16,9 @@ void newTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bool
 
 void endTask(std::vector<time_t> &taskTime, bool &activeTask)
 {
-        std::time_t current_time = std::time(nullptr);
-        taskTime.back() = current_time - taskTime.back();
-        activeTask = false;
+    std::time_t current_time = std::time(nullptr);
+    taskTime.back() = (std::time_t)(std::difftime(current_time, taskTime.back()));
+    activeTask = false;
 }
 
 void printTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bool &activeTask)
@@ -28,27 +29,8 @@ void printTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bo
         for(int i = 0; i < task.size() - (int)activeTask; i++)
         {
             std::cout << "TASK: " << task[i] << std::endl;
-            int daySecond = 86400;
-            int hourSecond = 3600;
-            int minuteSecond = 60;
-            int tempTime = (int)taskTime[i];
-            std::cout << "Task execution time";
-            if(tempTime > daySecond)
-            {
-                std::cout << " days: " << tempTime / daySecond;
-                tempTime -= (tempTime/daySecond) * daySecond;
-            }
-            if(taskTime[i] > 3600)
-            {
-                std::cout << " hours: " << tempTime / hourSecond;
-                tempTime -= (tempTime/hourSecond) * hourSecond;
-            }
-            if(taskTime[i] > 60)
-            {
-                std::cout << " minutes: " << tempTime / minuteSecond;
-                tempTime -= (tempTime/minuteSecond) * minuteSecond;
-            }
-            std::cout << " seconds: " << tempTime << std::endl;
+            std::tm *a = std::gmtime(&taskTime[i]);
+            std::cout << std::put_time(a, "%H:%M:%S") << std::endl;
         }
     }
     else std::cout << "No tasks saved!" << std::endl;
@@ -59,29 +41,19 @@ int main() {
     std::vector<std::string> task;
     std::vector<std::time_t> taskTime;
     bool activeTask = false;
-    while(true)
-    {
+    while (true) {
         std::cout << "Enter the command: begin, end, status or exit:";
         std::cin >> command;
-        if(command == "begin")
-        {
-            if(activeTask)
-            {
-                endTask(taskTime, activeTask);
-            }
+        if (command == "begin") {
+            if (activeTask) endTask(taskTime, activeTask);
             newTask(task, taskTime, activeTask);
-        }
-        else if(command == "end")
-        {
-            if(activeTask) endTask(taskTime, activeTask);
+        }else if (command == "end") {
+            if (activeTask) endTask(taskTime, activeTask);
             else std::cout << "No active task!" << std::endl;
-        }
-        else if(command == "status") printTask(task, taskTime, activeTask);
-        else if(command == "exit")
-        {
+        }else if (command == "status") printTask(task, taskTime, activeTask);
+        else if (command == "exit") {
             std::cout << "Goodbye!!!";
             return 0;
-        }
-        else std::cout << "Input is not correct! Try again!!!" << std::endl;
+        } else std::cout << "Input is not correct! Try again!!!" << std::endl;
     }
 }
