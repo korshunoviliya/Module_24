9c031d954f605291fbd02087a1fb7a0a3b494b52
korshunov80@mail.ru <korshunov80@mail.ru>
2021-11-13 13:58:05 +0300

    Update module_24_1.cpp
    add struct Task instead of two vectors

--- module_24_1.cpp
+++ module_24_1.cpp
@@ -4,32 +4,39 @@
 #include <vector>
 #include <iomanip>
 
-void newTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bool &activeTask)
+struct Task{
+    std::string task_name{"unknown task"};
+    std::time_t task_time{std::time(nullptr)};
+};
+void newTask(std::vector<Task> &task, bool &activeTask)
 {
+    static int counter{0};
     std::string new_task;
     std::cout << "Enter the new task:";
     std::cin >> new_task;
-    task.push_back(new_task);
-    taskTime.push_back(std::time(nullptr));
+    task.emplace_back();
+    task[counter].task_name = new_task;
+    task[counter].task_time = std::time(nullptr);
     activeTask = true;
+    ++counter;
 }
 
-void endTask(std::vector<time_t> &taskTime, bool &activeTask)
+void endTask(std::vector<Task> &task, bool &activeTask)
 {
     std::time_t current_time = std::time(nullptr);
-    taskTime.back() = (std::time_t)(std::difftime(current_time, taskTime.back()));
+    task.back().task_time = (std::time_t)(std::difftime(current_time, task.back().task_time));
     activeTask = false;
 }
 
-void printTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bool &activeTask)
+void printTask(std::vector<Task> &task, bool &activeTask)
 {
     if(!task.empty())
     {
-        if(activeTask) std::cout << task.back() << std::endl;
+        if(activeTask) std::cout << task.back().task_name << std::endl;
         for(int i = 0; i < task.size() - (int)activeTask; i++)
         {
-            std::cout << "TASK: " << task[i] << std::endl;
-            std::tm *a = std::gmtime(&taskTime[i]);
+            std::cout << "TASK: " << task[i].task_name << std::endl;
+            std::tm *a = std::gmtime(&task[i].task_time);
             std::cout << std::put_time(a, "%H:%M:%S") << std::endl;
         }
     }
@@ -37,20 +44,20 @@ void printTask(std::vector<std::string> &task, std::vector<time_t> &taskTime, bo
 }
 
 int main() {
+    std::vector<Task> task;
     std::string command;
-    std::vector<std::string> task;
-    std::vector<std::time_t> taskTime;
     bool activeTask = false;
+
     while (true) {
         std::cout << "Enter the command: begin, end, status or exit:";
         std::cin >> command;
         if (command == "begin") {
-            if (activeTask) endTask(taskTime, activeTask);
-            newTask(task, taskTime, activeTask);
+            if (activeTask) endTask(task, activeTask);
+            newTask(task, activeTask);
         }else if (command == "end") {
-            if (activeTask) endTask(taskTime, activeTask);
+            if (activeTask) endTask(task, activeTask);
             else std::cout << "No active task!" << std::endl;
-        }else if (command == "status") printTask(task, taskTime, activeTask);
+        }else if (command == "status") printTask(task, activeTask);
         else if (command == "exit") {
             std::cout << "Goodbye!!!";
             return 0;
