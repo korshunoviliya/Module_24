e1ca01c343b3844b6fdc10f0ed2bcf6b18a3b608
korshunov80@mail.ru <korshunov80@mail.ru>
2021-11-13 15:13:48 +0300

    Update module_24_2.cpp
    add struct & add sorting
    not ready

--- module_24_2.cpp
+++ module_24_2.cpp
@@ -5,44 +5,62 @@
 #include <string>
 #include <vector>
 
-void inputR(std::vector<std::tm> &birthday_list)
+struct Birthday_list{
+    std::tm birthday{};
+    std::string name;
+};
+void input_birthday_list(std::vector<Birthday_list> &birthday_list, std::string &name)
 {
+    static int counter{0};
     std::time_t t = std::time(nullptr);
     std::tm *birth_day = std::localtime(&t);
     std::cout << "Enter the birthday in format YEAR/MONTH/DAY: ";
     std::cin >> std::get_time(birth_day, "%Y/%m/%d");
-    birthday_list.push_back(*birth_day);
+    birthday_list.emplace_back();
+    birthday_list[counter].name = name;
+    birthday_list[counter].birthday = *birth_day;
+    ++counter;
 }
 
-void reminders (std::vector<std::tm> &birthday_list, std::vector<std::string> &listName)
+void reminders (std::vector<Birthday_list> &birthday_list)
 {
-    std::time_t t = std::time(nullptr);
-    std::tm *now = std::localtime(&t);
-    //std::tm *temp = now;
-    for(int i = 0; i < birthday_list.size(); ++i){
-        if(now->tm_mon == birthday_list[i].tm_mon &&
-            now->tm_mday == birthday_list[i].tm_mday) {
-            std::cout << "Today is " << listName[i] << "`s Birthday\n";
+    bool check{false};
+    std::time_t t {std::time(nullptr)};
+    std::tm *now {std::localtime(&t)};
+    Birthday_list temp {*now,""};
+    for(auto & i : birthday_list){
+        if(temp.birthday.tm_mon == i.birthday.tm_mon &&
+            temp.birthday.tm_mday == i.birthday.tm_mday) {
+            std::cout << "Today is " << i.name << "`s Birthday\n";
         }
-    //
+        if(i.birthday.tm_mon == now->tm_mon
+            && i.birthday.tm_mday <= temp.birthday.tm_mday
+            && i.birthday.tm_mday >= now->tm_mday){
+            temp = i;
+            check = true;
+        }
+        else if(!check
+                && i.birthday.tm_mon > temp.birthday.tm_mon
+                && i.birthday.tm_mday < temp.birthday.tm_mday){
+            temp = i;
+        }
     }
+    std::cout << temp.name << "'s next birthday "
+            << temp.birthday.tm_mday << "/"
+            << temp.birthday.tm_mon << "/"
+            << temp.birthday.tm_year << std::endl;
 }
 
 int main() {
-    std::vector<std::tm> birthday_list;
-    std::vector<std::string> listName;
 
+    std::vector<Birthday_list> birthday_list;
     while(true) {
-
         std::cout << "Enter the name: ";
         std::string name;
         std::cin >> name;
-        if (name == "end") reminders(birthday_list, listName);
+        if (name == "end") reminders(birthday_list);
         else if(name == "exit") return 0;
-        else{
-            listName.push_back(name);
-            inputR(birthday_list);
-        }
+        else input_birthday_list(birthday_list, name);
     }
 }
 
